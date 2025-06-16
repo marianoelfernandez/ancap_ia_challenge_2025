637bb28c683c0ecc5e27f85324d8b96939649d0b
Santiago Morón Santos <santimoron001@gmail.com>
2025-06-16 15:54:24 -0300

    Updated model

--- backend/llm-service/services/agent.py
+++ backend/llm-service/services/agent.py
@@ -31,7 +31,7 @@ class Agent():
             google_api_key=settings.api_key
             )
         self.pro_agent =ChatGoogleGenerativeAI(
-            model="gemini-2.5-pro-preview-06-05",
+            model="gemini-2.5-flash-preview-05-20",
             temperature=0,
             google_api_key=settings.api_key
             )
