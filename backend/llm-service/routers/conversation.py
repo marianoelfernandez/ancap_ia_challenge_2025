9a00f070f1f342891623ee09d57d4f06aa3e65c8
Santiago Morón Santos <santimoron001@gmail.com>
2025-06-19 10:02:25 -0300

    Add recommender endpoint

--- backend/llm-service/routers/conversation.py
+++ backend/llm-service/routers/conversation.py
@@ -4,9 +4,11 @@ from pydantic import ValidationError,BaseModel
 from utils.auth import get_user_id_from_auth
 import logging
 from services.agent import Agent
+from services.chart_agent import ChartAgent
 
 router = APIRouter()
 agent = Agent()
+chart_agent = ChartAgent()
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
@@ -15,6 +17,12 @@ class QueryRequest(BaseModel):
     query: str
     conversation_id: Optional[str] = None
     auth_payload: Optional[dict] = None
+
+
+class ChartRequest(BaseModel):
+    natural_query: str
+    data_output: Optional[str] = None
+    sql_query: Optional[str] = None
 
 @router.post("/query")
 def receive_query_endpoint(req:QueryRequest,  authorization: str = Header(...)):
@@ -28,5 +36,18 @@ def receive_query_endpoint(req:QueryRequest,  authorization: str = Header(...)):
         raise HTTPException(422, ve.errors())
     except Exception as e:
         logger.error(f"Unexpected error with query: {str(e)}", exc_info=True)
+        raise HTTPException(500, f"{e}")
+
+@router.post("/chart")
+def receive_chart_endpoint(req:ChartRequest,  authorization: str = Header(...)):
+    logger.debug("Received chart suggestion request")
+    try:
+        tables = chart_agent.ask_agent(req.natural_query, req.data_output[:100], req.sql_query)
+        return {"response": tables}
+    except ValidationError as ve:
+        logger.warning(f"Validation error: {ve.errors()}")
+        raise HTTPException(422, ve.errors())
+    except Exception as e:
+        logger.error(f"Unexpected error with chart suggestions: {str(e)}", exc_info=True)
         raise HTTPException(500, f"{e}")
 
