7ef8e77bcea8946875cbc7e532d4470e90da2e89
Santiago Morón Santos <santimoron001@gmail.com>
2025-06-20 23:45:55 -0300

    Improved agent memory

--- backend/llm-service/utils/constants.py
+++ backend/llm-service/utils/constants.py
@@ -314,12 +314,11 @@ PK: (FacPlaId, FacTpoDoc, FacSerie, FacNro, FacLinNro)
 """
 data_dictionary_incomplete_prompt = ChatPromptTemplate.from_messages([
     """Eres un experto en diccionario de datos, usa el diccionario de datos para traducir la pregunta del usuario a una
-      pregunta curada con información específica sobre las tablas a consultar, debes REESCRIBIR la consulta del usuario EN LENGUAJE NATURAL para que sea más descriptiva, sin agregar preguntas para el usuario.
+      pregunta curada con información específica sobre las tablas a consultar, generalmente son sobre el sistema de entregas o de facturas. Debes REESCRIBIR la consulta del usuario EN LENGUAJE NATURAL para que sea más descriptiva, sin agregar preguntas para el usuario.
       Responde SOLO con la consulta transformada o una solicitud de más información comenzando con [RETRY] seguido de una pregunta sobre la inforacion ambigua o casos en donde la pregunta no haga referencia a ninguna tabla.
       NO fuerces al usuario a realizar un filtro por fechas\n\n""",
     ("system", "Usa el siguiente diccionario de datos para responder a las preguntas del usuario: {data_dictionary}"),
-    MessagesPlaceholder("chat_history"),
-    "{chat_history}\n\n"
+    "Este es el historial de mensajes previos:",MessagesPlaceholder("chat_history"),
     "Input: {query}\n"])
 
 
